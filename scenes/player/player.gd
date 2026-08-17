22165b78b6ec6ffb0a4e0bb046cb5861fa93b2ab
Noah Peters <97405316+noahpete@users.noreply.github.com>
2026-08-16 22:02:01 -0700

    Init commit with player movement

---
+++ scenes/player/player.gd
@@ -0,0 +1,24 @@
+class_name Player
+extends CharacterBody2D
+
+@export var max_speed: float = 60.0
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+
+func _physics_process(_delta: float) -> void:
+	var input_vector: Vector2 = Input.get_vector("left", "right", "up", "down")
+
+	if input_vector.length() > 0:
+		animation_player.play("walk")
+
+		if input_vector.x > 0:
+			sprite_2d.flip_h = false
+		else:
+			sprite_2d.flip_h = true
+	else:
+		animation_player.play("RESET")
+
+	velocity = input_vector * max_speed
+	move_and_slide()
