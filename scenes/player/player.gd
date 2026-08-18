dfac217d3218cd1c456ad6f38cf7adf2ec97b661
Noah Peters <97405316+noahpete@users.noreply.github.com>
2026-08-18 13:06:25 -0700

    Add AnimationTree to smooth walking animation transtition

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,19 +1,25 @@
 class_name Player
 extends CharacterBody2D
 
+const BLEND_SPEED: float = 8.0
+
 @export var max_speed: float = 60.0
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var animation_tree: AnimationTree = $AnimationTree
+@onready var visuals: Node2D = $Visuals
 
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	var input_vector: Vector2 = Input.get_vector("left", "right", "up", "down")
-	if input_vector.length() > 0:
-		animation_player.play("walk")
-		sprite_2d.flip_h = input_vector.x < 0
-	else:
-		animation_player.play("RESET")
+
+	var target_blend: float = 1.0 if input_vector.length() > 0.0 else 0.0
+	var current_blend: float = animation_tree["parameters/blend_position"]
+	animation_tree["parameters/blend_position"] = move_toward(current_blend,
+		target_blend, BLEND_SPEED * delta)
+
+	if input_vector.x != 0:
+		visuals.scale.x = -1 if input_vector.x < 0 else 1
 
 	velocity = input_vector * max_speed
 	move_and_slide()
